f2f2f077513ded5817112cedf73d02fa24e150ae
Matthias Bohlen <mbohlen@mbohlen.de>
2025-05-17 14:17:49 +0200

    Refactored: AccountRepository doesn't need trx parameter anymore.

--- packages/accounting/src/adapter/persistence/AccountRepositoryImpl.ts
+++ packages/accounting/src/adapter/persistence/AccountRepositoryImpl.ts
@@ -1,66 +1,70 @@
+import { withTransactionFor } from '@ddd-video-club-v2/database';
 import { Model } from 'objection';
+
 import type { Account, Entry } from '../../domainmodel/Account';
 import { createEntryForAccount, updateBalanceInAccount } from '../../domainmodel/Account';
 import type { AccountRepository } from '../../domainmodel/AccountRepository';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface AccountModel extends Account {}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface EntryModel extends Entry {}
 
 class EntryModel extends Model {
-	static get tableName() {
-		return 'dddvc.account_entries';
-	}
+    static get tableName() {
+        return 'dddvc.account_entries';
+    }
 }
 
 class AccountModel extends Model {
-	static get tableName() {
-		return 'dddvc.accounts';
-	}
+    static get tableName() {
+        return 'dddvc.accounts';
+    }
 
-	static get idColumn() {
-		return 'id';
-	}
+    static get idColumn() {
+        return 'id';
+    }
 
-	static relationMappings = {
-		entries: {
-			relation: Model.HasManyRelation,
-			modelClass: EntryModel,
-			join: {
-				from: 'dddvc.accounts.id',
-				to: 'dddvc.account_entries.accountId',
-			},
-		},
-	};
+    static relationMappings = {
+        entries: {
+            relation: Model.HasManyRelation,
+            modelClass: EntryModel,
+            join: {
+                from: 'dddvc.accounts.id',
+                to: 'dddvc.account_entries.accountId',
+            },
+        },
+    };
 
-	constructor() {
-		super();
-		this.createEntry = createEntryForAccount(this);
-		this.updateBalance = updateBalanceInAccount(this);
-	}
+    constructor() {
+        super();
+        this.createEntry = createEntryForAccount(this);
+        this.updateBalance = updateBalanceInAccount(this);
+    }
 }
 
-export const AccountRepositoryImpl: AccountRepository = {
-	async addEntryToAccount(trx, entry) {
-		return EntryModel.query(trx).insert(entry).returning('*');
-	},
+export const AccountRepositoryImpl = withTransactionFor<AccountRepository>((trx) => ({
+    async addEntryToAccount(entry) {
+        return EntryModel.query(trx).insert(entry).returning('*');
+    },
 
-	async getAccountByCustomerId(trx, customerId) {
-		return AccountModel.query(trx).findOne({ customerId }).withGraphFetched('entries');
-	},
+    async getAccountByCustomerId(customerId) {
+        return AccountModel.query(trx).findOne({ customerId }).withGraphFetched('entries');
+    },
 
-	async updateAccount(trx, accountId, accountInput) {
-		return AccountModel.query(trx).updateAndFetchById(accountId, accountInput);
-	},
+    async updateAccount(accountId, accountInput) {
+        return AccountModel.query(trx).updateAndFetchById(accountId, accountInput);
+    },
 
-	async createAccount(trx, accountInput) {
-		return AccountModel.query(trx).insert(accountInput).returning('*');
-	},
+    async createAccount(accountInput) {
+        return AccountModel.query(trx).insert(accountInput).returning('*');
+    },
 
-	async findAccounts(trx) {
-		return AccountModel.query(trx);
-	},
+    async findAccounts() {
+        return AccountModel.query(trx);
+    },
 
-	async getAccountById(trx, accountId) {
-		return AccountModel.query(trx).findById(accountId);
-	},
-};
+    async getAccountById(accountId) {
+        return AccountModel.query(trx).findById(accountId);
+    },
+}));
